d6d1dfd1e999a7bd93c1c7b23c85cd0d193bc47f
Lydia Gorham <gorhamlydia@gmail.com>
2016-07-08 04:23:09 +0000

    Incorporated isdigit method

--- decode_challenge.py
+++ decode_challenge.py
@@ -1,28 +1,15 @@
-def convert_to_list(encoded_message_string):
-    """Converts the encoded message into a list for easier type evaluation"""
-    
-    as_list = []
-    for char in encoded_message_string:
-        try:
-            int_char = int(char)
-            as_list.append(int_char)
-        except:
-            as_list.append(char)
-    return as_list
-
 
 def decode(encoded_message):
     """Decode a string
 
-    Each number tells you how many characters to skip before finding a good letter
-    After each good letter should come the next number"""
+    Each number tells you how many characters to skip before finding a letter 
+    we want to print. After each printed letter should come the next number."""
     
     decoded_message = ""
-    list_message = convert_to_list(encoded_message)
-    for item in list_message:
-        if type(item) is int:
-            list_message = list_message[(item + 1):]
-            decoded_message = decoded_message + list_message[item]
+    for i in encoded_message:
+        if encoded_message.isdigit(i) == True:
+            encoded_message = encoded_message[(int(i) + 1):]
+            decoded_message = decoded_message + encoded_message[item]
     print decoded_message
 
 decode("0h1ae2bcy")
